45f57fd46acfe28fc5dd831f2d366fc92765b3cf
Ranger9103 <kateheise@yahoo.com>
2022-05-26 12:46:13 -0500

    module done

--- app.py
+++ app.py
@@ -8,19 +8,18 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-#define the route for the HTML page
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
-#route for the button of the web application
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-
+   
 if __name__ == "__main__":
    app.run()
+
